74dd2ae95892acd664f84d1cbe27c32c0e919209
anapy <anabelenfunesc@gmail.com>
2024-07-16 13:56:16 +0200

    giving new style to page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Book Journal</h1>
+      <h1>
+        <span className="letter">B</span>
+        <span className="letter">O</span>
+        <span className="letter">O</span>
+        <span className="letter">K</span>
+        <span className="letter">J</span>
+        <span className="letter">O</span>
+        <span className="letter">U</span>
+        <span className="letter">R</span>
+        <span className="letter">N</span>
+        <span className="letter">A</span>
+        <span className="letter">L</span>
+      </h1>
       <button className="main-button" type="button">See Books</button>
       <button type="button">New Book</button>
       <button type="button">Add Review</button>
